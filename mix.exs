6b73ba6545449ae9ef317a2fdda1a407a46ead80
TzeYiing <ty@tzeyiing.com>
2026-04-19 19:53:30 +0800

    chore: version bump

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule PhoenixTestJsdom.MixProject do
   use Mix.Project
 
-  @version "0.1.1"
+  @version "0.1.2"
   @source_url "https://github.com/ziinc/phoenix_test_jsdom"
 
   def project do
